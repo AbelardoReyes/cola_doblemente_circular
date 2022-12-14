56ac238cc794b9e577e77e42947a4ed611792953
Abelardo <abelardoreyes256@gmail.com>
2022-12-13 19:26:06 -0600

    Primer commit

---
+++ Cola -Doble-Circular-V2/Cola.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cola__Doble_Circular_V2
+{
+    class Cola
+    {
+        int TOP = 5;
+        int CERO = 0;
+        int final = -1, inicio = 4;
+        int PI = 5, PF = -1;
+        int[] dato { set; get; }
+        public void Inicializar()
+        {
+            dato = new int[TOP];
+        }
+        //Encolar Inicio
+        public void encolarInicio(int Num)
+        {
+            if (PI == 5 && PF == -1)
+            {
+                PF++;
+                dato[PF] = Num;
+                return;
+            }
+            if (PF + 1 == PI)
+            {
+                Console.WriteLine("La cola esta llena");
+                Console.ReadKey();
+                return;
+            }
+            if (PI == CERO)
+            {
+                PI = TOP;
+                //Console.WriteLine("SIUUUUUUUUUUUu");
+            }
+            PI--;
+            dato[PI] = Num;/*
+            Console.WriteLine("PI: " + PI);
+            Console.WriteLine("PF: " + PF);*/
+            Console.ReadKey();
+        }
+        //Desencolar Inicio
+        public void desencolarInicio()
+        {
+            if (PI == TOP)
+            {
+                PI = CERO;
+            }
+            if (PI == PF)
+            {
+                PI = 5;
+                PF = -1;
+                Console.WriteLine("Los indices han coincidido");
+                Console.ReadKey();
+                return;
+            }
+            //
+            dato[PI] = CERO;
+            PI++;/*
+            Console.WriteLine("PI: " + PI);
+            Console.WriteLine("PF: " + PF);*/
+            Console.ReadKey();
+        }
+        //Encolar Final
+        public void encolarFinal(int Num)
+        {
+            if (PF + 1 == PI)
+            {
+                Console.WriteLine("La cola esta llena");
+                Console.ReadKey();
+                return;
+            }
+            if (PF == inicio)
+            {
+                PF = final;
+                //Console.WriteLine("SIUUUUUUUUUUUu");
+            }
+            //
+            if (PI == PF)
+            {
+                PF++;
+                dato[PF] = Num;
+                return;
+            }
+            PF++;
+            dato[PF] = Num;/*
+            Console.WriteLine("PI: " + PI);
+            Console.WriteLine("PF: " + PF);*/
+            Console.ReadKey();
+        }
+        //Desencolar Final
+        public void desencolarFinal()
+        {
+            if (PF == -1)
+            {
+                PF = inicio;
+            }
+            //
+            if (PI == PF)
+            {
+                PI = 5;
+                PF = -1;
+                Console.WriteLine("Los indices han coincidido");
+                Console.ReadKey();
+                return;
+            }/*
+            Console.WriteLine("PI: " + PI);
+            Console.WriteLine("PF: " + PF);*/
+            PF--;
+            Console.ReadKey();
+        }
+        //Mostrar
+        public void mostrarDatos()
+        {
+            if (PF < PI)
+            {
+                for (int i = PI; i <= inicio; i++)
+                {
+                    Console.WriteLine("|" + dato[i] + "|");
+                }
+                for (int i = 0; i <= PF; i++)
+                {
+                    Console.WriteLine("|"+dato[i]+"|");
+                }/*
+                Console.WriteLine("PI: " + PI);
+                Console.WriteLine("PF: " + PF);*/
+                Console.ReadKey();
+                return;
+            }
+            else if (PF >= PI)
+            {
+                for (int i = PI; i <= PF; i++)
+                {
+                    Console.WriteLine("|" + dato[i] + "|");
+                }/*
+                Console.WriteLine("PI: " + PI);
+                Console.WriteLine("PF: " + PF);*/
+                Console.ReadKey();
+                return;
+            }/*
+            Console.WriteLine("PI: " + PI);
+            Console.WriteLine("PF: " + PF);*/
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Cola -Doble-Circular-V2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cola__Doble_Circular_V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num;
+            Cola Cola1 = new Cola();
+            Cola1.Inicializar();
+            int op;
+            do
+            {
+                Console.WriteLine("-----Menu de opciones-----");
+                Console.WriteLine("1)-Encolar Final");
+                Console.WriteLine("2)-Encolar Inicio");
+                Console.WriteLine("3)-Desencolar Final");
+                Console.WriteLine("4)-Desencolar Inicio");
+                Console.WriteLine("5)-Mostrar Cola");
+                Console.WriteLine("6)-Creditos, SIU!");
+                Console.WriteLine("0)-Salir");
+                Console.WriteLine();
+                Console.Write("Elige una opcion: ");
+                op = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("----------------------------");
+                switch (op)
+                {
+                    case 1:
+                        Console.Write("Ingresa un numero: ");
+                        Num = Convert.ToInt16(Console.ReadLine());
+                        Cola1.encolarFinal(Num);
+                        break;
+                    case 2:
+                        Console.Write("Ingresa un numero: ");
+                        Num = Convert.ToInt16(Console.ReadLine());
+                        Cola1.encolarInicio(Num);
+                        break;
+                    case 3:
+                        Cola1.desencolarFinal();
+                        break;
+                    case 4:
+                        Cola1.desencolarInicio();
+                        break;
+                    case 5:
+                        Cola1.mostrarDatos();
+                        break;
+                    case 6:
+                        Console.WriteLine("------Creditos-----");
+                        Console.WriteLine("Cristian Sebastian Avitia");
+                        Console.WriteLine("Abelardo Garcia Reyes");
+                        Console.WriteLine("4-A");
+                        Console.ReadKey();
+                        break;
+                    default:
+                        Console.WriteLine("Opcion no valida!");
+                        break;
+                }
+                Console.WriteLine();
+                Console.Clear();
+            } while (op != 0);
+        }
+    }
+}
